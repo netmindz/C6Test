4de097707c8a69ba77d30d75d4e7b54dd11ddc56
Will Tatam <will@netmindz.net>
2024-12-09 20:24:45 +0000

    pioarduino

--- src/C6Test.cpp
+++ src/C6Test.cpp
@@ -1,20 +1,23 @@
+//#define FASTLED_ESP32_I2S 1
 #include <FastLED.h>
 // #include <I2SClocklessLedDriver.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <ArduinoJson.h>
+// #include <AsyncTCP.h>
+// #include <ESPAsyncWebServer.h>
+// #include <ArduinoJson.h>
 
-AsyncWebServer ws(80);
-JsonDocument doc;
+// AsyncWebServer ws(80);
+// JsonDocument doc;
 CRGB leds[1];
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Setup");
-    FastLED.addLeds<NEOPIXEL, 9>(leds, 1);
+    FastLED.addLeds<NEOPIXEL, 8>(leds, 1);
 }
 void loop() {
     Serial.print(".");
+    leds[0] = CRGB::Blue;
+    FastLED.delay(1000);
     leds[0] = CRGB::Red;
-    FastLED.show();
+    FastLED.delay(1000);
 }
